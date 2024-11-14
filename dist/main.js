539c8c6e5c025814537da9c8da5ed038778ea8b8
phatommobb <nickoluoch1130@gmail.com>
2024-11-14 19:54:45 +0300

    code

--- dist/main.js
+++ dist/main.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _greeting__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./greeting */ \"./src/greeting.js\");\n\r\n\r\nconsole.log(_greeting__WEBPACK_IMPORTED_MODULE_0__.greeting);\r\n\n\n//# sourceURL=webpack://webpack-revisited/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _greeting__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./greeting */ \"./src/greeting.js\");\n\n\nconsole.log(_greeting__WEBPACK_IMPORTED_MODULE_0__.greeting);\n\n\n//# sourceURL=webpack://webpack-revisited/./src/index.js?");
 
 /***/ })
 
